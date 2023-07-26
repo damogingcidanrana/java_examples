2bbbf4ab442446560bed0f0ad5ecae889ba1cb07
ssidorova <ssidorova@sportmaster.ru>
2023-07-26 11:00:00 +0300

    fibonacci

--- Fibonacci/src/ru/ssidorova/fibonacci/Main.java
+++ Fibonacci/src/ru/ssidorova/fibonacci/Main.java
@@ -2,6 +2,14 @@ package ru.ssidorova.fibonacci;
 
 public class Main {
     public static void main(String[] args) {
-        
+        int i = 1;
+        int j = 1;
+
+        while (i < 1000) {
+            System.out.println(i);
+            final int c = j;
+            j = i + j;
+            i = c;
+        }
     }
 }

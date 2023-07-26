4919e3ad760a042346d5307e36c1df02be2beb94
ssidorova <ssidorova@sportmaster.ru>
2023-07-26 12:59:31 +0300

    sorting names

---
+++ SortingNames/src/ru/ssidorova/sorting/names/Main.java
@@ -0,0 +1,26 @@
+package ru.ssidorova.sorting.names;
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        String input;
+        final Scanner scanner = new Scanner(System.in);
+        List<String> names = new ArrayList<String>();
+
+        do {
+            System.out.println("Введите имя");
+            input = scanner.nextLine();
+            names.add(input);
+        } while (!input.isEmpty());
+
+        Collections.sort(names);
+
+        for (String p: names) {
+            System.out.println(p);
+        }
+    }
+}
